5e951b0927d38bd340d0b0f8ac5c7d91212513de
Carlos <carlos.bdez.esc@gmail.com>
2023-02-21 22:08:09 +0100

    feat: add logout with blocklist

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_smorest import Api
 
 import models
+from blocklist import BLOCKLIST
 from db import db
 from resources.dog import blp as DogBlueprint
 from resources.user import blp as UserBlueprint
@@ -64,6 +65,19 @@ def create_app(db_url=None):
                     "description": "Request does not contain an access token.",
                     "error": "authorization_required",
                 }
+            ),
+            401,
+        )
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
             ),
             401,
         )
